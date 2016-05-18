ce6713e158bc36f0e7b5a3e4d5e1f1ff35fe5468
Angelo Bovino <angelo.bovino@outlook.com>
2016-05-17 23:31:42 -0400

    Javascript Functions YT Practice

---
+++ jscript-functions.js
@@ -0,0 +1,10 @@
+//piece of code that does one or more actions
+//DRY = Don't Repeat Yourself
+
+//while do for loops
+
+var myList = ["Apples", "Oranges", "Bananas"]
+
+for (var i = 0; i < myList.length; i++) {
+	alert("you have "+myList[i]+" in your basket");
+}
